23ef38883a18aded76d93a4163405e63b9bce63d
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-16 21:30:38 -0300

    remove problema com loop infinito no relacionamento one to many de User

--- src/main/java/com/challenge/entity/User.java
+++ src/main/java/com/challenge/entity/User.java
@@ -1,58 +1,76 @@
 package com.challenge.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
 
     @Column
     @NotNull
     @Size(max = 100)
+    @Getter
+    @Setter
     private String fullName;
 
     @Column
     @Email
     @Size(max = 100)
     @NotNull
+    @Getter
+    @Setter
     private String email;
 
     @Column
     @NotNull
     @Size(max = 50)
+    @Getter
+    @Setter
     private String nickname;
 
     @Column
     @NotNull
     @Size(max = 255)
+    @Getter
+    @Setter
     private String password;
 
     @Column
-    @CreatedDate
+    @CreationTimestamp
+    @Getter
+    @Setter
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "id.user")
+    @OneToMany(mappedBy = "id.user", fetch = FetchType.LAZY)
     private List<Candidate> candidates;
 
-    @OneToMany(mappedBy = "id.user")
+    @OneToMany(mappedBy = "id.user", fetch = FetchType.LAZY)
     private List<Submission> submissions;
 }
