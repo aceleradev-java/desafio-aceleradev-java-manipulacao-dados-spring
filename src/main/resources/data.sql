ea50942b9cd21cbb1eb08eee7a4d3b2001b11607
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-16 21:30:00 -0300

    adiciona arquivo com dados inciciais para o banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+insert into users ( email , full_name , nickname , password ) values ('email1@email.com', 'name full 1', 'nick1', '1231');
+insert into users ( email , full_name , nickname , password ) values ('email2@email.com', 'name full 2', 'nick2', '1232');
+insert into users ( email , full_name , nickname , password ) values ('email3@email.com', 'name full 3', 'nick3', '1233');
+insert into users ( email , full_name , nickname , password ) values ('email4@email.com', 'name full 4', 'nick4', '1234');
+insert into users ( email , full_name , nickname , password ) values ('email5@email.com', 'name full 5', 'nick5', '1235');
+insert into users ( email , full_name , nickname , password ) values ('email6@email.com', 'name full 6', 'nick6', '1236');
+insert into users ( email , full_name , nickname , password ) values ('email7@email.com', 'name full 7', 'nick7', '1237');
+
+insert into acceleration (name, slug) values ('acc 1', 'aaa');
+insert into acceleration (name, slug) values ('acc 2', '22222');
+
+
+insert into company  (name, slug) values ('company 1', 'c111');
+insert into company  (name, slug) values ('company 2', 'c222');
+
+insert into candidate   (user_id , company_id , acceleration_id , status ) values (1,1,1,1);
+insert into candidate   (user_id , company_id , acceleration_id , status ) values (1,1,2,1);
+insert into candidate   (user_id , company_id , acceleration_id , status ) values (2,2,1,1);
